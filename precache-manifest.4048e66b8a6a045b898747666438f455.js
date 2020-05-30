e975fe62e472b4132d25a26bb93bccfa2c01c497
codydeny <codydeny@gmail.com>
2020-05-31 00:42:30 +0530

    Updates

---
+++ precache-manifest.4048e66b8a6a045b898747666438f455.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05faab28b2271d9d074d0975e8515ac9",
+    "url": "/freepaper/index.html"
+  },
+  {
+    "revision": "389e3cb83051d0086dab",
+    "url": "/freepaper/static/css/2.40a775a0.chunk.css"
+  },
+  {
+    "revision": "c2c4e1a7d5a90e9d0118",
+    "url": "/freepaper/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "389e3cb83051d0086dab",
+    "url": "/freepaper/static/js/2.4c4ca3e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/freepaper/static/js/2.4c4ca3e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2c4e1a7d5a90e9d0118",
+    "url": "/freepaper/static/js/main.5c9a85eb.chunk.js"
+  },
+  {
+    "revision": "77b005147d53aba7b6a9",
+    "url": "/freepaper/static/js/runtime-main.06a7eba3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/freepaper/static/media/logo.5d5d9eef.svg"
+  }
+]);
